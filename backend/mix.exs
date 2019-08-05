b01029f623490d521471958cbd1fdb20b072f8cb
Dan Quan <dan@quan.io>
2019-08-05 12:32:10 -0700

    Add warning-as-errors

--- backend/mix.exs
+++ backend/mix.exs
@@ -10,7 +10,8 @@ defmodule BudgetSH.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      elixirc_options: [warnings_as_errors: true]
     ]
   end
 
