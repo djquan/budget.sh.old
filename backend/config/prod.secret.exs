c8ed39721417f145dbf4948c2d1e6a45942ea1b6
Dan Quan <dan@quan.io>
2019-08-06 07:21:22 -0700

    Add releases, prepare for prod

--- backend/config/prod.secret.exs
+++ backend/config/prod.secret.exs
@@ -23,12 +23,21 @@ config :budgetsh, BudgetSHWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
   secret_key_base: secret_key_base
 
+session_salt =
+  System.get_env("SESSION_SALT") ||
+    raise """
+    environment variable SESSION_SALT is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :budgetsh, :sessions, salt: session_salt
+
 # ## Using releases (Elixir v1.9+)
 #
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :budgetsh, BudgetSHWeb.Endpoint, server: true
+config :budgetsh, BudgetSHWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
