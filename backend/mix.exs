b70692e608269ecba93537fc21e68fc892cfbeaf
Dan Quan <dan@quan.io>
2019-08-17 08:57:41 -0700

    Add dialyzer

--- backend/mix.exs
+++ backend/mix.exs
@@ -11,7 +11,8 @@ defmodule BudgetSH.MixProject do
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
-      elixirc_options: [warnings_as_errors: true]
+      elixirc_options: [warnings_as_errors: true],
+      dialyzer: [plt_add_deps: :transitive]
     ]
   end
 
