acf0b342fc6d2e276cf50e1af4c2f68c596f960f
Dan Quan <dan@quan.io>
2019-08-17 17:02:01 -0700

    Remoe some comments

--- backend/lib/budgetsh/application.ex
+++ backend/lib/budgetsh/application.ex
@@ -6,24 +6,17 @@ defmodule BudgetSH.Application do
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
-      # Start the Ecto repository
       BudgetSH.Repo,
-      # Start the endpoint when the application starts
       BudgetSHWeb.Endpoint
       # Starts a worker by calling: BudgetSH.Worker.start_link(arg)
       # {BudgetSH.Worker, arg},
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: BudgetSH.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   def config_change(changed, _new, removed) do
     BudgetSHWeb.Endpoint.config_change(changed, removed)
     :ok
