f9c354b48595fe0509d188557fb53aaa2445974c
Dan Quan <dan@quan.io>
2019-10-04 08:16:28 -0700

    Convert signin to hooks

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,9 +1,8 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import gql from "graphql-tag";
-import { Mutation } from "react-apollo";
-import { RouteComponentProps } from "react-router-dom";
+import { useMutation } from "react-apollo";
+import { useHistory } from "react-router-dom";
 import { GET_CURRENT_USER_QUERY } from "../components/Header";
-
 import Error from "../components/Error"
 import Loading from "../components/Loading"
 
@@ -18,84 +17,77 @@ const SIGNIN_MUTATION = gql`
   }
 `;
 
-class SignIn extends Component<RouteComponentProps> {
-  state = {
+const SignIn = () => {
+  const [state, setState] = useState({
     email: "",
     password: ""
-  };
+  });
 
-  handleChange = (event: { target: { name: any; value: any; }; }) => {
+  const history = useHistory();
+
+  const handleChange = (event: { target: { name: any; value: any; }; }) => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    setState({ ...state, ...{ [name]: value } });
   };
 
-  isFormValid = () => {
+  const isFormValid = () => {
     return (
-      this.state.email.length > 0 &&
-      this.state.password.length > 0
+      state.email.length > 0 &&
+      state.password.length > 0
     );
   };
 
-  handleCompleted = (data: { signin: { session: string; }; }) => {
-    localStorage.setItem("auth-token", data.signin.session);
+  const [signIn, { loading: mutationLoading, error: mutationError }] = useMutation(
+    SIGNIN_MUTATION,
+    {
+      update(cache, { data: { signin: { user } } }) {
+        cache.writeQuery({
+          query: GET_CURRENT_USER_QUERY,
+          data: { me: user }
+        });
+      },
+      onCompleted({ signin: { session } }) {
+        localStorage.setItem("auth-token", session);
+        history.push("/");
+      },
+    }
+  )
 
-    this.props.history.push("/");
-  };
+  if (mutationLoading) return <Loading />;
 
-  handleUpdate = (cache: { writeQuery: (arg0: { query: any; data: { me: any; }; }) => void; }, { data }: any) => {
-    cache.writeQuery({
-      query: GET_CURRENT_USER_QUERY,
-      data: { me: data.signin.user }
-    });
-  };
-
-  render(): React.ReactNode {
-    return (
-      <Mutation
-        mutation={SIGNIN_MUTATION}
-        variables={this.state}
-        onCompleted={this.handleCompleted}
-        update={this.handleUpdate}>
-        {(signin: () => void, { loading, error }: any) => {
-          if (loading) return <Loading />;
-
-          return (
-            <form
-              className="signin"
-              onSubmit={e => {
-                e.preventDefault();
-                signin();
-              }}>
-              <Error error={error} />
-              <fieldset>
-                <label htmlFor="email">Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  required
-                  value={this.state.email}
-                  onChange={this.handleChange}
-                />
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  required
-                  value={this.state.password}
-                  onChange={this.handleChange}
-                />
-                <button type="submit" disabled={!this.isFormValid()}>
-                  Sign In
-                </button>
-              </fieldset>
-            </form>
-          );
-        }}
-      </Mutation>
-    );
-  }
+  return (
+    <form
+      className="signin"
+      onSubmit={e => {
+        e.preventDefault();
+        signIn({ variables: state });
+      }}>
+      {mutationError && <Error error={mutationError} />}
+      <fieldset>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          value={state.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          required
+          value={state.password}
+          onChange={handleChange}
+        />
+        <button type="submit" disabled={!isFormValid()}>
+          Sign In
+        </button>
+      </fieldset>
+    </form>
+  );
 }
 
 export default SignIn
